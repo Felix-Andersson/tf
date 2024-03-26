a991143fb2af0b2c34cba152200faabe541c6f88
Felix-Andersson <felix.andersson3@elev.ga.ntig.se>
2024-03-26 11:54:00 +0100

    v.13

--- app.rb
+++ app.rb
@@ -18,6 +18,8 @@ enable :sessions
 
 #Vad är regular expressions i Validerings pp:n ?? Fråga Emil
 
+#Lägg till .to_s på alla params
+
 before('/protected/*') do
     if (session[:id] == nil)
         #Användare har inte loggat in och försöker nå en sida förutom '/' och '/showregister'
@@ -40,7 +42,6 @@ post('/login') do       #ENDA SOM ÄR KVAR ÄR ATT FIXA MED TID MELLAN LOGIN (FI
 
     if BCrypt::Password.new(password_digest) == password
         session[:id] = id
-        session[:username] = username
         session[:role] = role
         redirect('/protected/home') #Här redirectar vi 
     else
@@ -173,6 +174,35 @@ get('/protected/gods/:id') do
     db = connect_database()
     @result = db.execute("SELECT * FROM god WHERE id = ?",god_id).first
     @result2 = db.execute("SELECT mythology.name FROM god INNER JOIN mythology ON god.mythology_id = mythology.id WHERE god.id = ?",god_id).first
+    @comment_result = db.execute("SELECT * FROM comment WHERE god_id = ?", god_id)
+    @comment_result2 = db.execute("SELECT user.username FROM comment INNER JOIN user ON comment.user_id = user.id WHERE comment.god_id = ?", god_id)
     slim(:'gods/show')
 end
 
+get('/protected/profile/edit') do
+    user_id = session[:id]
+    db = connect_database()
+    @result = db.execute("SELECT * FROM user WHERE id = ?", user_id).first
+    slim(:profile)
+end
+
+post('/protected/profile/update') do
+    username = params[:username]
+    bio = params[:bio]
+    
+    db = SQLite3::Database.new("db/database.db")
+    db.execute("UPDATE user SET username = ?,bio = ?",username,bio)
+    redirect('/protected/profile/edit')
+end
+
+post('/protected/comment/new') do
+    content = params[:content].to_s
+    god_id = params[:god_id].to_i
+    user_id = session[:id]
+    time = Time.new
+    date = "#{time.year}-#{time.month}-#{time.day}"
+
+    db = SQLite3::Database.new("db/database.db")
+    db.execute("INSERT INTO comment (user_id, god_id, date, content) VALUES (?,?,?,?)", user_id, god_id, date, content)
+    redirect("/protected/gods/#{god_id}")
+end
